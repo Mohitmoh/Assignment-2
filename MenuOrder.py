22661c2357b2dfbbab937821a714aeb1c201c56c
Mohitmoh <91853423+Mohitmoh@users.noreply.github.com>
2021-11-03 01:48:17 +0530

    created list for dinner item

--- MenuOrder.py
+++ MenuOrder.py
@@ -5,13 +5,13 @@
 # Description:
 # This program take required input from user for Food delivery
 
-# Function: userDetails
+# Function: userDetailsFun
 # Description: will take inputs from user 
 # Parameters: 
 #          none
 # Return value:
 #          userDetails: details of user
-def userDetails():
+def userDetailsFun():
     userDetails = []  # list for adding details of user
     userDetails.append(input("Enter your First Name : "))
     userDetails.append(input("Enter your Last Name : "))
@@ -24,69 +24,90 @@ def userDetails():
     userDetails.append(input("Enter your Phone Number : "))
     return userDetails
 
+# Function: itemListFun
+# Description: will create list for dinner item
+# Parameters: 
+#          none
+# Return value:
+#          itemList: list of dinner item
+def itemListFun():
+    itemList = {
+        "1":"Chicken",
+        "2":"Salmon",
+        "3":"Zucchini",
+        "4":"Pasta",
+        "5":"Cauliflower",
+        "6":"Pizza"
+    }
+    return itemList
+
+# Function: costListFun
+# Description: will create list for cost of dinner item
+# Parameters: 
+#          none
+# Return value:
+#          costList: list of cost 
+def costListFun():
+    costList = [15,20,18,10,13,15]
+    return costList
+
 # Function: listItem
 # Description: will print list of dinner item
 # Parameters: 
-#          dinnerItem1 : name of 1st dinner item
-#          dinnerItem2 : name of 2nd dinner item
-#          costItem1   : cost of 1st dinner item
-#          costItem2   : cost of 2nd dinner item 
+#          itemList : list of dinner item
+#          costList : list of dinner item's cost
 # Return value:
 #           none
 
-def listItem(dinnerItem1, dinnerItem2, costItem1, costItem2):
-    print("There are Two types of meals are available : \n{0:<25s}{1:<25s}{2:25s}\n{3:<25s}{4:<25s}${5:<25d}\n{6:<25s}{7:<25s}${8:<25d}".format(
-        'Sr. No.', 'Meals', 'Cost', '1', dinnerItem1, costItem1, '2', dinnerItem2, costItem2))
-
-# Function: srNumber
+def listItem(itemList,costList):
+    print("{0:<25s}{1:<25s}{2:25s}".format('Sr. No.', 'Meals','Cost'))
+    index = 0 # for printig price list
+    for key in itemList:
+        print("{:<25s}{:<25s}${:<25d}".format(key,itemList[key],costList[index]))
+        index = index + 1
+# Function: srNumberFun
 # Description: will take the input of Sr. Number for dinner item
 # Parameters: 
 #          none 
 # Return value:
 #           Sr. number 
-def srNumber():
+def srNumberFun():
     return int(input("which serial number meals you want ? "))
 
-# Function: orderPrint
+# Function: orderPrintFun
 # Description: will print order details
 # Parameters: 
 #          srNumber : Sr. number chosed by user
-#          dinnerItem1 : name of 1st dinner item
-#          dinnerItem2 : name of 2nd dinner item
+#          itemList: list of dinner item
 # Return value:
 #           none
-def orderPrint(srNumber,dinnerItem1,dinnerItem2):
-    print("Your orders is : ", end="")
-    if srNumber == 1:
-        print(dinnerItem1)
-    elif srNumber == 2:
-        print(dinnerItem2)
+def orderPrintFun(srNumber,itemList):
+    print("Your orders is : ", itemList[str(srNumber)])
+    
 
-# Function: studentCheck
+# Function: studentCheckFun
 # Description: will check user is student or not
 # Parameters: 
 #          None
 # Return value:
 #          studentCheck : user is student or not
-def studentCheck():
+def studentCheckFun():
     studentCheck = input("you get 10% discount if you are a student. You are a student Y/N ")
     if studentCheck.lower() == 'y' or studentCheck.lower() == 'n':
         return studentCheck
     else:
         print("select the correct option")
-        studentCheck()
+        studentCheckFun()
 
 # Function: itemPrice
 # Description: will calculate price of total item
 # Parameters: 
-#          srNumber : Sr. Number
+#          costList: list of dinner item's cost
+#           amount: amount of dinner
 # Return value:
 #          itemPrice: price of total item
-def itemPrice(costItem1,costItem2,amount):
-    if srNumber == 1:
-        itemPrice = costItem1*amount
-    elif srNumber == 2:
-        itemPrice = costItem2*amount
+def itemPriceFun(costList,amount):
+    itemPrice = costList[srNumber+1]*amount
     return itemPrice
 
 # Function: discount
@@ -95,7 +116,7 @@ def itemPrice(costItem1,costItem2,amount):
 #          studentCheck: check user is student or not
 # Return value:
 #          discount: discount for student
-def discount(studentCheck,itemPrice):
+def discountFun(studentCheck,itemPrice):
     # 10% discount if user is student
     # Calculation for user is student or not
     if studentCheck.lower() == 'y':
@@ -112,16 +133,16 @@ def discount(studentCheck,itemPrice):
 #          discount: discount for student
 # Return value:
 #          subTotal: sub total
-def subTotal(itemPrice,discount):
+def subTotalFun(itemPrice,discount):
     subTotal = itemPrice - discount     # Sub Total
     return subTotal
 
-def tax(subTotal):
+def taxFun(subTotal):
     # Calculation for 13% Tax
     tax = subTotal*0.13
     return tax
 
-def totalAmount(subTotal,tax):
+def totalAmountFun(subTotal,tax):
     # total prices
     totalAmount = subTotal + tax
     return totalAmount
@@ -137,7 +158,7 @@ def totalAmount(subTotal,tax):
 #       city: city
 #       province : province
 #       postalCode: postal code
-def userInformation(userDetails):
+def userInformationFun(userDetails):
     print("{} {}".format(userDetails[0], userDetails[1]))
     print("Address: {},{},{}".format(userDetails[2], userDetails[3], userDetails[4]))
     print("{:>9s}{},{},{}".format('', userDetails[6], userDetails[7], userDetails[8]))
@@ -149,32 +170,30 @@ def userInformation(userDetails):
 print("-----WELCOME TO ARNOLD'S AMAZING EATS-----")
 
 # taken user and address details from user
-userDetails = userDetails()
+userDetails = userDetailsFun()
 
 # taken order from user
-dinnerItem1 = "Pizza"  # Dinner Items
-dinnerItem2 = "Bargger"
-costItem1 = 10              # Cost of Dinner Item
-costItem2 = 8
+itemList = itemListFun() # list of dinner item
+costList = costListFun() # list of dinner's cost
 
 while True:  # infinte while loop
     while True:
 
         # printing list of dinner items
-        listItem(dinnerItem1,dinnerItem2,costItem1,costItem2)
+        listItem(itemList,costList)
         
         # take Sr. Number from user
-        srNumber = srNumber()
+        srNumber = srNumberFun()
 
         # checking Sr. No. is correct or not
-        if srNumber == 1 or srNumber == 2:
+        if srNumber in [1,2,3,4,5,6]:
             amount = int(input("Enter the amount of meals : "))
             break
         else:
             print("Please Enter correct serial no.")
 
     # priting order of user
-    orderPrint(srNumber, dinnerItem1, dinnerItem2)
+    orderPrintFun(srNumber, itemList)
 
     # Confrimation from user about order
     confirm = input("Confrim your order Y/N ")
@@ -186,30 +205,26 @@ while True:  # infinte while loop
 
 
 # Checking user is Student or Not
-studentCheck = studentCheck()
+studentCheck = studentCheckFun()
 
 # Calculation for placing order
-itemPrice = itemPrice(costItem1, costItem2, amount)
-discount = discount(studentCheck, itemPrice)
-subTotal = subTotal(itemPrice, discount)
-tax = tax(subTotal)
-totalAmount = totalAmount(subTotal,tax)
+itemPrice = itemPriceFun(costList, amount)
+discount = discountFun(studentCheck, itemPrice)
+subTotal = subTotalFun(itemPrice, discount)
+tax = taxFun(subTotal)
+totalAmount = totalAmountFun(subTotal,tax)
 # *********** printing recipts ***********
 
 # printing user information
-userInformation(userDetails)
+userInformationFun(userDetails)
 
 # another method for writing recipts
 # print("\n{0} {1}\nAddress: {2},{3},{4}\n{14:>9s}{5},{6},{7}\n{8:>40s}{9:>20s}\n{10:<10s}{11:>30s}{12:>20s}{13:>20s}\n{14:-<10s}{14:->30s}{14:->20s}{14:->20s}".format(firstName,lastName,streetNumber,streetName,unit,city,province,postalCode,'Item','Item','Order','Amt','Price','Total',''))
 
 
 # printing dinner item that selected by user and also printing some details about prices
-if srNumber == 1:
-    print("{:<10s}{:>30d}{:>20s}{:>20s}\n".format(dinnerItem1, amount, '$' +
-          str("{:.2f}".format(costItem1)), '$'+str("{:.2f}".format(itemPrice))))
-else:
-    print("{:<10s}{:>30d}{:>20s}{:>20s}\n".format(dinnerItem2, amount, '$' +
-          str("{:.2f}".format(costItem2)), '$'+str("{:.2f}".format(itemPrice))))
+print("{:<10s}{:>30d}{:>20s}{:>20s}\n".format(itemList[str(srNumber)], amount, '$' + str("{:.2f}".format(costList[srNumber-1])), '$'+str("{:.2f}".format(itemPrice))))
+
 
 # printing discount details if user is a student
 if studentCheck.lower() == 'y':
@@ -217,6 +232,7 @@ if studentCheck.lower() == 'y':
           '-$'+str("{:.2f}".format(discount))))
 else:
     pass
+
 
 # printing details about tax and total bill price
 print("{:>60s}{:>20s}\n{:>60s}{:>20s}\n{:>80s}\n{:>60s}{:>20s}".format('Sub Total', '$'+str("{:.2f}".format(subTotal)),
